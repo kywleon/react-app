77c8bb1346af52cb5878ae89f2984fb20e18b864
Kwok Yew Weng <66321555+kywleon@users.noreply.github.com>
2024-07-03 17:26:17 +0800

    Update the css

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,8 +7,10 @@ export default defineConfig({
   base: "/react-app/",
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "/src"),
-      "~@": path.resolve(__dirname, "/src"),
+      '@': '/src',
     },
+  },
+  build: {
+    assetsDir: 'assets',
   },
 })
