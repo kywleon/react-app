0710de6f7e20acbdc037f447d9263f84821f60f3
Kwok Yew Weng <66321555+kywleon@users.noreply.github.com>
2024-07-03 17:30:20 +0800

    Undo the css bug

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-app/",
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-  build: {
-    assetsDir: 'assets',
-  },
+  base: "/react-app/"
 })
